df8e4935db07768a781a70ff987ccdf18d34919a
TVIEIL <tvieil59@gmail.com>
2026-05-15 00:25:11 +0200

    Add copyright and license information to the script
    Added copyright notice and licensing information.

--- modules/ear/bouche_receveur_final_v1_0.py
+++ modules/ear/bouche_receveur_final_v1_0.py
@@ -1,3 +1,21 @@
+# Copyright 2026 Thierry VIEIL
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# ==============================================================================
+# PROJET NATACHA - MODULE GSTREAM FLUX AUDIO SYNTHESE VOCALE VERS MICRO-CASQUE
+# ==============================================================================
+
 # ==============================================================================
 # PROJET NATACHA - NŒUD OREILLE (Ryzen 5)
 # Script : bouche_receveur_final_v1_0.py (v1.0-SR)
